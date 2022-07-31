56d09d140cec44329a7685bb75128179d1511122
影连 <a946547732@163.com>
2022-07-31 16:24:01 +0000

    1

--- python/1.py
+++ python/1.py
@@ -19,5 +19,21 @@
 
 # print(data)
 
-import sys
-sys.stdout.write(' h1 ')
+# import sys
+# sys.stdout.write(' h1 ')
+
+# print(1,2)
+
+# import sys
+# for i in sys.argv:
+#     print('i', i)
+# print('path', sys.path)
+
+# from sys import argv, path
+# print(argv, path)
+
+a = b = 2
+print(a, b)
+
+a, b = 1, 2
+print(a, b)
