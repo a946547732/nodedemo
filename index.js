9aeae3f7de91e63eac55014bd5f39548375a1fc4
影连 <a946547732@163.com>
2022-07-31 09:34:36 +0000

    http

---
+++ index.js
@@ -0,0 +1,13 @@
+// 引入http模块
+const http = require('http')
+// 创建web服务器实例
+const server = http.createServer()
+server.on('request', function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.write("你好啊")
+    res.end();
+})
+
+server.listen(3000, function () {
+    console.log('服务器运行中 地址 http://127.0.0.1:3000');
+})
