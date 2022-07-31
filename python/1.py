b36e0cd4b7a5efb94d3426eda22c4089f6069071
影连 <a946547732@163.com>
2022-07-31 16:08:32 +0000

    1

--- python/1.py
+++ python/1.py
@@ -1,14 +1,23 @@
-import keyword
-import a2
+# import keyword
+# import a2
 
-print(keyword.kwlist)
+# print(keyword.kwlist)
 
-if True:
-    print('true')
-else:
-    print('false')
-print(123)
+# if True:
+#     print('true')
+# else:
+#     print('false')
+# print(123)
 
-print('''qqq
-222
-''')
+# print('''qqq
+# 222
+# ''')
+
+# str = '1234567890'
+
+# data = input('请输入：')
+
+# print(data)
+
+import sys
+sys.stdout.write(' h1 ')
